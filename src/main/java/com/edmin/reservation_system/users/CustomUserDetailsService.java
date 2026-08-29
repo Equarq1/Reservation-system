01fb314cc937c4c0a3a0377b7d42e935fdc02203
Equarq1 <gaifullineduard@mail.ru>
2026-08-29 13:25:36 +0300

    feat(security): implement CustomUserDetailsService and base SecurityConfig beans

--- src/main/java/com/edmin/reservation_system/users/CustomUserDetailsService.java
+++ src/main/java/com/edmin/reservation_system/users/CustomUserDetailsService.java
@@ -1,4 +1,26 @@
 package com.edmin.reservation_system.users;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserEntity user = userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Пользователь не найден"));
+        return new User(user.getEmail(), user.getPassword(), List.of(new SimpleGrantedAuthority(user.getRole())));
+    }
 }
