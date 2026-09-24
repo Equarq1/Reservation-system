6a8e12dcea94d3449c9608b5ae84a40e64ce968b
Equarq1 <gaifullineduard@mail.ru>
2026-09-24 15:07:12 +0300

    fix(auth): add validation constraints to LoginRequest DTO

--- src/main/java/com/edmin/reservation_system/auth/LoginRequest.java
+++ src/main/java/com/edmin/reservation_system/auth/LoginRequest.java
@@ -1,7 +1,15 @@
 package com.edmin.reservation_system.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+
+        @NotBlank
+        @Email
         String email,
+
+        @NotBlank
         String password
 ) {
 }
